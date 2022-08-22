f1903f375342d80f6544618aa7c2583ee44ff977
limyel <limyel@outlook.com>
2022-08-22 19:40:09 +0800

    all

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="basic-layout">
     <div class="nav-side">
-      <div class="log">
-        <span>浩元</span>
+      <div class="logo">
+        浩元
       </div>
       <el-menu
           default-active="2"
+          router
+          background-color="#122150"
+          text-color="#ffffff"
+          :collapse="false"
           class="nav-menu">
-        <el-menu-item index="4">
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><setting /></el-icon>系统管理
+          </template>
+          <el-menu-item index="1-1">用户管理</el-menu-item>
+          <el-menu-item index="1-2">菜单管理</el-menu-item>
+        </el-sub-menu>
+
+        <el-menu-item index="1">
           <el-icon><setting /></el-icon>
           <span>Navigator Four</span>
         </el-menu-item>
@@ -15,7 +27,13 @@
     </div>
     <div class="content-right">
       <div class="nav-top">
-
+        <div class="nav-left">
+          <div class="menu-fold">
+            <el-icon><Fold /></el-icon>
+          </div>
+          <div class="bread">面包屑</div>
+        </div>
+        <div class="user-">用户</div>
       </div>
       <div class="wrapper">
         <div class="main-page">
@@ -42,6 +60,18 @@ export default {
     overflow-y: auto;
     transition: width .5s;
     background-color: #122150;
+
+    .logo {
+      text-align: center;
+      font-size: 20px;
+      color: #ffffff;
+      padding: 15px 0;
+    }
+
+    .nav-menu {
+      height: calc(100vh - 50px);
+      border-right: none;
+    }
   }
   .content-right {
     margin-left: 200px;
@@ -52,6 +82,15 @@ export default {
       justify-content: space-between;
       border-bottom: 1px solid #ddd;
       padding: 0 20px;
+
+      .nav-left {
+        display: flex;
+        align-items: center;
+
+        .menu-fold {
+          margin-right: 15px;
+        }
+      }
     }
     .wrapper {
       background-color: #eeeeee;
